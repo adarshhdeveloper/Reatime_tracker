38abf994c948fd169b078b267ce5ce1cae0e5035
Adarsh Prajapati <154526910+adarshhdeveloper@users.noreply.github.com>
2026-08-30 15:01:12 +0000

    created a map and added tilelayer

--- public/js/script.js
+++ public/js/script.js
@@ -1,3 +1,25 @@
 //io initilization 
 const socket = io()
-console.log("hey")
+
+if(navigator.geolocation){
+    navigator.geolocation.watchPosition((position)=>{
+       const {latitude , longitude} = position.coords
+       socket.emit("send-location",{latitude , longitude})
+    },
+    (error) =>{
+        console.error(error)
+    },
+    {
+        enableHighAccuracy : true,
+        timeout:5000,         //every 5 sec me location check 
+        maximumAge:0
+    }
+)
+}
+
+const map = L.map("map").setView([0,0],10)
+
+ //ye map dikhane ke liye
+L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"),{
+    attribution : "Adarsh ke PAPA ka map " // koi bhai name de sakte hii ham 
+}.addTO(map)
